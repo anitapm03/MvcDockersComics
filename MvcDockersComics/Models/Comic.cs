070fc179869711b05d6ae82ad76fee33b85567af
anitapm03 <apereiramolinero@gmail.com>
2024-04-22 14:13:17 +0200

    .

--- MvcDockersComics/Models/Comic.cs
+++ MvcDockersComics/Models/Comic.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcDockersComics.Models
 {
-    [Table("Comics")]
+    [Table("COMICS")]
     public class Comic
     {
         [Key]
